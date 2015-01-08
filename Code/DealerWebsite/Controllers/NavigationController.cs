97c0197cc4fef3e7b8b0dbb0206988edbe3ad0d2
Nathan Townsend <ntownsend2@mt.gov>
2015-01-08 16:28:38 -0700

    Prototyping

---
+++ Code/DealerWebsite/Controllers/NavigationController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DealerWebsite.Controllers
+{
+    public class NavigationController : Controller
+    {
+
+        public ActionResult MainNav()
+        {
+            return PartialView();
+        }
+
+    }
+}
---
+++ Code/DealerWebsite/Controllers/ServicesController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DealerWebsite.Controllers
+{
+    public class ServicesController : Controller
+    {
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Service(int Id)
+        {
+            return View();
+        }
+
+    }
+}
