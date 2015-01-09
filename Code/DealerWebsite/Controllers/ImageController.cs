64b278fa50a4dd362a3fb290f968c65a0f7555b3
Nathan Townsend <ntownsend2@mt.gov>
2015-01-09 11:44:57 -0700

    Continued Prototyping

---
+++ Code/DealerWebsite/Controllers/ImageController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DealerWebsite.Controllers
+{
+    public class ImageController : Controller
+    {
+
+        public ActionResult ImageGrid()
+        {
+            return PartialView();
+        }
+
+        public ActionResult ImageList()
+        {
+            return PartialView();
+        }
+
+    }
+}
