adc5a155472d68d9edd527e4b737721700642f56
Nathan Townsend <nathan@nathantownsend.com>
2015-01-20 23:23:17 -0700

    prototyped services and gallery pages
    also started filling in microdata

---
+++ Code/DealerWebsite/Controllers/StaffController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DealerWebsite.Controllers
+{
+    public class StaffController : Controller
+    {
+
+        public ActionResult List()
+        {
+            return PartialView();
+        }
+
+        public ActionResult Member()
+        {
+            return View();
+        }
+
+    }
+}
